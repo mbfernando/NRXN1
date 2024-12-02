304af67e7abd6db3cc3a32cc15ac4d74767ba0c9
atokolyi <alex.tokolyi@gmail.com>
2024-12-02 10:42:32 -0500

    Added differential splicing code

---
+++ differential_splicing/1_star_map.sh
@@ -0,0 +1,8 @@
+STAR --genomeDir /gpfs/commons/home/atokolyi/data/nrxn1_ref/GRCh38_and_mm10/star \
+        --readFilesIn ${in_dir}/${in_file}_1.fq.gz ${in_dir}/${in_file}_2.fq.gz \
+        --readFilesCommand zcat \
+        --outFileNamePrefix out/${in_file}/${in_file}_ \
+        --runThreadN 16 \
+        --outSAMtype BAM SortedByCoordinate \
+        --outSAMstrandField intronMotif \
+        --twopassMode Basic
